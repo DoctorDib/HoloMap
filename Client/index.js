10964765ac2256c67e98f3f8e03ed03229d5fafb
James Dibnah <jamesdibnah@gmail.com>
2024-10-10 22:11:09 +0100

    undoing dev mode

--- Client/index.js
+++ Client/index.js
@@ -18,7 +18,7 @@ const createWindow = () => {
         autoHideMenuBar: true,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
-            devTools: true
+            devTools: false
         },
     });
 
